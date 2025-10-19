1d6be122d8bbc9cd75ca131c4f2e7caf8ac0a861
Shripad-Joshi <joshishripad28@gmail.com>
2025-10-19 13:17:21 +0530

    pushing initial project setup to repo

---
+++ src/main/java/com/expo/managmentSystem/UserAndProductManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.expo.managmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndProductManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAndProductManagmentApplication.class, args);
+	}
+
+}
